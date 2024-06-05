198853d14883564c164a13bf570b6612baad4ba4
Ball <113112727+ball0982@users.noreply.github.com>
2024-06-05 17:06:57 +0700

    Add files via upload

---
+++ NormalUser.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Library;
+
+/**
+ *
+ * @author ball08
+ */
+public class NormalUser extends User {
+
+    public NormalUser(String userName){
+        super(userName);
+    }
+
+    public NormalUser(String userName, String password){
+        super(userName,password);
+    }
+
+
+}
